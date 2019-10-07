9298cf18fca461695864493f50da94ebff8d6599
Bolaji <bolaji1050@gmail.com>
2019-10-07 23:15:38 +0800

    feat(home): landing feature added

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,3 +1,5 @@
 <template>
-  <v-parallax app src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" class="" height="600"></v-parallax>
+  <v-parallax app src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" class="mt-n8" height="100%">
+    <h1 class="display-4 text-center">Maze Design Labs</h1>
+  </v-parallax>
 </template>
