82ac46ca70fcf2f48694d10247ff6bd499d14879
Bolaji <bolaji1050@gmail.com>
2019-10-19 04:24:41 +0800

    fix(home): partners images fixed

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -6,12 +6,57 @@
       hide-delimiter-background
       show-arrows-on-hover
       delimiter-icon="mdi-checkbox-blank-outline">
-    <v-carousel-item v-for="(partner, index) in partners" :key="index" class="">
+    <v-carousel-item class="">
       <v-sheet color="teal" height="100%" class="py-16">
         <v-col class="fill-height" align="center" justify="center" >
-          <div class="display-3 white--text text-center mb-20">{{ partner.title }}</div>
-          <v-img class="mb-20" src=""></v-img>
-          <div class="title white--text text-centers px-8 body-1">{{partner.content}}</div>
+          <div class="display-3 white--text text-center mb-8">ROYAL THAI</div>
+          <v-img
+            src="../assets/PartnerLogo/royal-thai.png"
+            max-height="250"
+            max-width="250">
+          </v-img>
+          <div class="title white--text text-centers px-8 body-1">{{thai}}</div>
+        </v-col>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item class="">
+      <v-sheet color="teal" height="100%" class="py-16">
+        <v-col class="fill-height" align="center" justify="center" >
+          <div class="display-3 white--text text-center mb-20">SPEED WHEELS</div>
+          <v-img
+            src="../assets/PartnerLogo/speedwheels.png"
+            max-height="250"
+            max-width="250"
+            class="mb-20">
+          </v-img>
+          <div class="title white--text text-centers px-8 body-1">{{speed}}</div>
+        </v-col>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item class="">
+      <v-sheet color="teal" height="100%" class="py-16">
+        <v-col class="fill-height" align="center" justify="center" >
+          <div class="display-3 white--text text-center mb-20">OHJUICE MALAYSIA</div>
+          <v-img
+            src="../assets/PartnerLogo/ohjuice.png"
+            max-height="250"
+            max-width="250">
+          </v-img>
+          <div class="title white--text text-centers px-8 body-1">{{juice}}</div>
+        </v-col>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item class="">
+      <v-sheet color="teal" height="100%" class="py-16">
+        <v-col class="fill-height" align="center" justify="center" >
+          <div class="display-3 white--text text-center mb-20">EDUCAID</div>
+          <v-img
+            src="../assets/PartnerLogo/educaid.png"
+            max-height="250"
+            max-width="250"
+            class="mb-20">
+          </v-img>
+          <div class="title white--text text-centers px-8 body-1">{{educaid}}</div>
         </v-col>
       </v-sheet>
     </v-carousel-item>
@@ -24,12 +69,10 @@ export default {
   name: 'partner',
   data() {
     return {
-      partners: [
-        { img: '../assets/PartnerLogo/speedwheels.png', title: 'SPEED WHEELS', content: 'Speedwheels provides cars for Rental, Purchase and Repair services. They are the only company in Malaysia providing interest free installmnent packages. ' },
-        { img: '../assets/PartnerLogo/ohjuice.png', title: 'OHJUICE MALAYSIA', content: 'Ohjuice sells premium quality, fresh fruit smoothies that are delicious and healthy.' },
-        { img: '../assets/PartnerLogo/royal-thai.png', title: 'ROYAL THAI', content: 'Royal Thai Travel and Trading provide memorable tours of Thailand, catering particularly to Muslim tourists. If you are a muslim looking to explore thailand, go with Royal Thai and you won\'t have to worry about looking halal food and family friendly tour spots. ' },
-        { img: '../assets/PartnerLogo/educaid.png', title: 'EDUCAID', content: 'Educaid serves to privde aid in acedamic tasks, particularly with things that might not be in your skillset. They help design diagrams, produce 3D models, and correct language on research papers.' },
-      ],
+      educaid: 'Educaid serves to privde aid in acedamic tasks, particularly with things that might not be in your skillset. They help design diagrams, produce 3D models, and correct language on research papers.',
+      juice: 'Ohjuice sells premium quality, fresh fruit smoothies that are delicious and healthy.',
+      speed: 'Speedwheels provides cars for Rental, Purchase and Repair services. They are the only company in Malaysia providing interest free installmnent packages.',
+      thai: 'Royal Thai Travel and Trading provide memorable tours of Thailand, catering particularly to Muslim tourists. If you are a muslim looking to explore thailand, go with Royal Thai and you won\'t have to worry about looking halal food and family friendly tour spots.',
     };
   },
 };
